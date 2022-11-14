95f8508c88a4378a3aa788545e9c1f718363d21f
Samuel <fnsamuel31@gmail.com>
2022-11-14 11:47:02 -0600

    Initial Commit

---
+++ SideMenuSFuentes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SideMenuSFuentes
+//
+//  Created by MacBookMBA1 on 14/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
